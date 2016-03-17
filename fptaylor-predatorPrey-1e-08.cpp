8e133085788bb50cb7936bdef802f1b63b04cfc1
wfchiang <weifan.wf@gmail.com>
2016-03-16 18:34:05 -0600

    add benchmarks for predatorPrey

---
+++ fptaylor-predatorPrey-1e-08.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char **argv) {
+	double x = 0.114223352544;
+
+	for (int ii = 0 ; ii < 10000000 ; ii++) {
+	double ____expr_0 = ((((double)4.0 * x) * x) / ((double)1.0 + ((x / (double)1.11) + (x / (double)1.11))));
+
+}
+
+	return 0;
+}
---
+++ fptaylor-predatorPrey-5e-07.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char **argv) {
+	float x = 0.136636813383;
+
+	for (int ii = 0 ; ii < 10000000 ; ii++) {
+	float ____expr_0 = ((((float)4.0 * x) * x) / ((float)1.0 + ((x / (float)1.11) + (x / (float)1.11))));
+
+}
+
+	return 0;
+}
