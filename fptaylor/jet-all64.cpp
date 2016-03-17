c8feb2f32b7d1b718adedc5822dc1676d7ea5111
wfchiang <weifan.wf@gmail.com>
2016-03-17 00:19:56 -0600

    add fptaylor benchmarks

---
+++ fptaylor/jet-all64.cpp
@@ -0,0 +1,24 @@
+
+// #include <iostream>
+#include <stdio.h> 
+
+#include "../emeasure.h" 
+
+using namespace std;
+
+int main (int argc, char ** argv) { 
+	double x2 = -0.261927065084;
+	double x1 = -4.79139328265;
+
+	assert(argc == 2); 
+	int sockfd = start_emeasure(string("jet-0-0001-ener-") + string(argv[1]) + ".csv"); 
+
+	for (int ii = 0 ; ii < 10000000 ; ii++) {
+	double ____expr_0 = (x1 + ((((((((((double)2.0 * x1) * (((((double)3.0 * (x1 * x1)) + ((double)2.0 * x2)) - x1) / ((x1 * x1) + (double)1.0))) * ((((((double)3.0 * (x1 * x1)) + ((double)2.0 * x2)) - x1) / ((x1 * x1) + (double)1.0)) - (double)3.0)) + ((x1 * x1) * (((double)4.0 * (((((double)3.0 * (x1 * x1)) + ((double)2.0 * x2)) - x1) / ((x1 * x1) + (double)1.0))) - (double)6.0))) * ((x1 * x1) + (double)1.0)) + (((double)3.0 * (x1 * x1)) * (((((double)3.0 * (x1 * x1)) + ((double)2.0 * x2)) - x1) / ((x1 * x1) + (double)1.0)))) + ((x1 * x1) * x1)) + x1) + ((double)3.0 * (((((double)3.0 * (x1 * x1)) + ((double)2.0 * x2)) - x1) / ((x1 * x1) + (double)1.0)))));
+
+}
+
+	stop_emeasure(sockfd); 
+
+	return 0;
+}
