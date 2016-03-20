4b95eaba39b3f7e216c80de97d286e4bb4480e66
Wei-Fan Chiang <wfchiang@wallace.cs.utah.edu>
2016-03-20 17:24:24 -0600

    update the modified file

--- micro/gaussian-x-dev-all64.cpp
+++ micro/gaussian-x-dev-all64.cpp
@@ -15,14 +15,14 @@ int main (int argc, char **argv) {
   // may need to sleep a little bit before here... 
   sleep(2); 
   assert(argc == 2); 
-  // int sockfd = start_emeasure(string("gauss-x-dev-all64-") + string(argv[1]) + ".csv"); 
+  int sockfd = start_emeasure(string("gauss-x-dev-all64-") + string(argv[1]) + ".csv"); 
 
 
   for (int ii = 0 ; ii < 100000000 ; ii++) {
     double ____expr_0 = (((double)1.0 / (dev * (double)2.50662827463)) * (exp2(((double)-1.0 * (((x - (double)0.0) * (x - (double)0.0)) / ((double)2.0 * (dev * dev)))))));
   }
 
-  // stop_emeasure(sockfd); 
+  stop_emeasure(sockfd); 
 
   return 0;
 }
