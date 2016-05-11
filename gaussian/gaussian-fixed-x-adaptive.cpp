2961a56cb46c219d10a9e4bfe44b307dbf6061ab
wfchiang <weifan.wf@gmail.com>
2016-05-11 15:10:14 -0600

    update gaussian

--- gaussian/gaussian-fixed-x-adaptive.cpp
+++ gaussian/gaussian-fixed-x-adaptive.cpp
@@ -15,28 +15,43 @@ int main (int argc, char **argv) {
   double __const_3 = 2.50662827463;
   double __const_2 = 0.0;
 
-  float rel32; 
-  double rel64; 
+  double ____expr_0; 
 
   for (int ii = 0 ; ii < 100000000 ; ii++) {
     
     if (dev <= 1.4) {
-      goto MIX2; 
+      if (dev <= 1.2) {
+	goto MIX2; 
+      }
+      else { // dev > 1.2
+	if (ave <= -0.4) {
+	  goto MIX2; 
+	}
+	else { // ave > -0.4
+	  goto MIX1; 
+	}
+      } 
     }
     else { // dev > 1.4 
       goto MIX0; 
     }
 
   MIX0:
-    rel64 = ((__const_4 / (double)(dev * (float)__const_3)) * (double)(exp2f(((float)__const_6 * ((((float)__const_2 - ave) * ((float)__const_2 - ave)) / ((float)__const_5 * (dev * dev)))))));
+    for (int ii = 0 ; ii < 100000000 ; ii++) {
+      ____expr_0 = ((__const_4 / (double)(dev * (float)__const_3)) * (double)(exp2f(((float)__const_6 * ((((float)__const_2 - ave) * ((float)__const_2 - ave)) / ((float)__const_5 * (dev * dev)))))));
+    }
     goto JoinPoint; 
 
   MIX1:
-    rel32 = ((float)(__const_4 / ((double)dev * __const_3)) * (exp2f(((float)__const_6 * ((((float)__const_2 - ave) * ((float)__const_2 - ave)) / ((float)__const_5 * (dev * dev)))))));
+    for (int ii = 0 ; ii < 100000000 ; ii++) {
+      ____expr_0 = ((__const_4 / ((double)dev * __const_3)) * (double)(exp2f(((float)__const_6 * ((((float)__const_2 - ave) * ((float)__const_2 - ave)) / ((float)__const_5 * (dev * dev)))))));
+    }
     goto JoinPoint; 
 
-  MIX2: 
-    rel64 = ((__const_4 / ((double)dev * __const_3)) * (exp2((double)((float)__const_6 * ((((float)__const_2 - ave) * ((float)__const_2 - ave)) / ((float)__const_5 * (dev * dev)))))));
+  MIX2:
+    for (int ii = 0 ; ii < 100000000 ; ii++) {
+      ____expr_0 = ((__const_4 / ((double)dev * __const_3)) * (exp2((double)((float)__const_6 * ((((float)__const_2 - ave) * ((float)__const_2 - ave)) / ((float)__const_5 * (dev * dev)))))));
+    }
     goto JoinPoint; 
 
   JoinPoint:
