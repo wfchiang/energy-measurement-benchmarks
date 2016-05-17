91ae23c27014ea1d58a90605ea5455f825639ac1
wfchiang <weifan.wf@gmail.com>
2016-05-17 16:46:16 -0600

    update gaussian

--- gaussian/gaussian-fixed-x-adaptive.cpp
+++ gaussian/gaussian-fixed-x-adaptive.cpp
@@ -9,6 +9,8 @@ int main (int argc, char **argv) {
   
   float ave = randFP32(-1.0, 1.0); 
   float dev = randFP32(1.0, 3.0); 
+  double ave64 = ave; 
+  double dev64 = dev; 
   
   double __const_6 = -1.0;
   double __const_5 = 2.0;
@@ -16,43 +18,40 @@ int main (int argc, char **argv) {
   double __const_3 = 2.50662827463;
   double __const_2 = 0.0;
 
-  double ____expr_0; 
+  float rel32; 
+  double rel64; 
+
 
   for (int ii = 0 ; ii < 100000000 ; ii++) {
     
-    if (dev <= 1.4) {
-      if (dev <= 1.2) {
-	goto MIX2; 
+    if (dev <= 1.6) {
+      if (dev <= 1.4) {
+	goto MIX0; 
       }
-      else { // dev > 1.2
-	if (ave <= -0.4) {
-	  goto MIX2; 
+      else { // dev > 1.4
+	if (ave <= -0.8) {
+	  goto MIX0;
 	}
-	else { // ave > -0.4
-	  goto MIX1; 
+	else { // ave > -0.8
+	  goto MIX2; 
 	}
       } 
     }
-    else { // dev > 1.4 
-      goto MIX0; 
+    else { // dev > 1.6 
+      goto MIX1; 
     }
 
+
   MIX0:
-    for (int ii = 0 ; ii < 100000000 ; ii++) {
-      ____expr_0 = ((__const_4 / (double)(dev * (float)__const_3)) * (double)(exp2f(((float)__const_6 * ((((float)__const_2 - ave) * ((float)__const_2 - ave)) / ((float)__const_5 * (dev * dev)))))));
-    }
+    rel64 = ((__const_4 / (dev64 * __const_3)) * (exp2((double)((float)__const_6 * ((((float)__const_2 - (float)ave64) * ((float)__const_2 - (float)ave64)) / ((float)__const_5 * ((float)dev64 * (float)dev64)))))));
     goto JoinPoint; 
 
   MIX1:
-    for (int ii = 0 ; ii < 100000000 ; ii++) {
-      ____expr_0 = ((__const_4 / ((double)dev * __const_3)) * (double)(exp2f(((float)__const_6 * ((((float)__const_2 - ave) * ((float)__const_2 - ave)) / ((float)__const_5 * (dev * dev)))))));
-    }
+    rel64 = ((double)((float)__const_4 / (dev * (float)__const_3)) * (double)(exp2f(((float)__const_6 * ((((float)__const_2 - ave) * ((float)__const_2 - ave)) / ((float)__const_5 * (dev * dev)))))));
     goto JoinPoint; 
 
   MIX2:
-    for (int ii = 0 ; ii < 100000000 ; ii++) {
-      ____expr_0 = ((__const_4 / ((double)dev * __const_3)) * (exp2((double)((float)__const_6 * ((((float)__const_2 - ave) * ((float)__const_2 - ave)) / ((float)__const_5 * (dev * dev)))))));
-    }
+    rel32 = ((float)(__const_4 / ((double)dev * __const_3)) * (exp2f(((float)__const_6 * ((((float)__const_2 - ave) * ((float)__const_2 - ave)) / ((float)__const_5 * (dev * dev)))))));
     goto JoinPoint; 
 
   JoinPoint:
