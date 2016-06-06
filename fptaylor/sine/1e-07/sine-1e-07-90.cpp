c2ed9b64b501cec3af22b31b1b0cddec07dd1a11
wfchiang <weifan.wf@gmail.com>
2016-06-06 12:28:50 -0600

    update sine

---
+++ fptaylor/sine/1e-07/sine-1e-07-90.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main (int argc, char **argv) {
+	double x = -1.0778215145;
+	double __const_2 = 5040.0;
+	double __const_1 = 120.0;
+	double __const_0 = 6.0;
+
+	for (int ii = 0 ; ii < 10000000 ; ii++) {
+	double ____expr_0 = (((x - (double)((float)((x * x) * x) / (float)__const_0)) + (double)(((float)(x * x) * (float)((x * x) * x)) / (float)__const_1)) - (double)(((float)(x * x) * ((float)(x * x) * (float)((x * x) * x))) / (float)__const_2));
+
+}
+
+	return 0;
+}
