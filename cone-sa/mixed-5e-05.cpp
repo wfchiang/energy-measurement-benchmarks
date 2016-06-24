7f13f245abbf0c33410e1bdbc2ced0c9e43dcd17
wfchiang <weifan.wf@gmail.com>
2016-06-24 14:29:45 -0600

    update cone-sa

--- cone-sa/mixed-5e-05.cpp
+++ cone-sa/mixed-5e-05.cpp
@@ -24,7 +24,7 @@ int main (int argc, char *argv[]) {
   FPT_2 h2   = (FPT_2)h * (FPT_2)h; 
   FPT_3 r2   = (FPT_3)r * (FPT_3)r; 
   FPT_4 hr2  = (FPT_4)h2 + (FPT_4)r2; 
-  FPT_5 pir  = PI * (FPT_5)r; 
+  FPT_5 pir  = (double)PI * (FPT_5)r; 
 
   FPT_6 tm1  = sqrtf((FPT_6)hr2); 
   FPT_7 tm2  = (FPT_7)r + (FPT_7)tm1; 
